5c91673c987c61dfa10c02c8f920043ae2fbfa68
Daniela <dtricolici12345@gmail.com>
2024-02-20 16:47:43 +0100

    back end 5

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const http = require('http');
 let requestCount = 0
 
 const server = http.createServer((req,res) => {
+     if (req.url === '/favicon.ico') {
+        res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+        res.end();
+        return;
+     }
     requestCount++
 
 switch(req.url) {
     case '/students': 
-    res.write("students") 
+    res.write("students" + requestCount) 
     break;
     case "/cours": 
     res.write("front+back")
@@ -16,7 +21,6 @@ switch(req.url) {
         res.write ('Not found ')
 }
 
-    res.write("  IT KAmasautra:" + requestCount )
     console.log(requestCount)
     res.end()
 })
